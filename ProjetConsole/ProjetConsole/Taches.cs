4b43444695489414276d244d4e14de40f6871042
lmilbor <lmilbor@LMILBOR17-DE>
2017-04-03 14:16:42 +0200

    MAJ Taches

--- ProjetConsole/ProjetConsole/Taches.cs
+++ ProjetConsole/ProjetConsole/Taches.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace ProjetConsole
 {
-    class Taches
+    public abstract class Taches
     {
+        #region Propriétés
+        public DateTime DateDébut { get; }
+        public TimeSpan DuréeTravailRéalisée { get; } //TODO A corriger
+        #endregion
+
+        #region Constructeurs
+
+        #endregion
+
+        #region Méthodes
+        public void Annulation()
+        {
+
+        }
+
+        #endregion
     }
+
+    public class TachesProductions :Taches
+    {
+        #region Propriétés
+        public TimeSpan DuréeTravailPrévue { get; }
+        public TimeSpan DuréeTravailRéstante { get; set; }
+        public bool TacheTerminée
+        {
+            get
+            {
+                if (DuréeTravailRéstante == TimeSpan.Zero)
+                    return true;
+                else
+                    return false;
+            }
+        } 
+        #endregion
+    }
+
+    public class TachesAnnexes :Taches
+    {
+        #region Propriétés
+        public TimeSpan DuréeRéaliséMensuel
+        {
+            get
+            {
+                if (DuréeTravailRéstante == TimeSpan.Zero)
+                    return true;
+                else
+                    return false;
+            }
+        }
+            #endregion
+        }
 }
