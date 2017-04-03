20573065301273832c0a3ee5533ee7a1ed4d4449
asauze <asauze@ASAUZE17-DE>
2017-04-03 14:10:46 +0200

    MAJ

--- ProjetConsole/ProjetConsole/DAL.cs
+++ ProjetConsole/ProjetConsole/DAL.cs
@@ -15,7 +15,7 @@ namespace ProjetConsole
         public Dictionary<double, Array> Version { get; set; } //TODO pour l'instant on affiche que des date -> qui à participé, etc.
         public List<Métiers> ListeMétiers { get; set; }
         public List<Personnes> ListePersonnes { get; set; } //TODO List ou Dictionary
-        public List<Tache> ListeTaches { get; set; } //TODO Faire un Dico ?
+        public List<Taches> ListeTaches { get; set; } //TODO Faire un Dico ?
         #endregion
         #region Constructeur
 
