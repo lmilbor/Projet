7b83e24594b553758b37108096554a674f6c1d79
lmilbor <lmilbor@LMILBOR17-DE>
2017-04-03 15:17:53 +0200

    Modification des TimeSpan en int, etc

--- ProjetConsole/ProjetConsole/Taches.cs
+++ ProjetConsole/ProjetConsole/Taches.cs
@@ -10,17 +10,22 @@ namespace ProjetConsole
     {
         #region Propriétés
         public DateTime DateDébut { get; }
-        public TimeSpan DuréeTravailRéalisé { get; }
+        public int DuréeTravailRéalisé { get; }
         #endregion
 
         #region Constructeurs
         public Taches()
         {
             DateDébut = DateTime.Today;
-            DuréeTravailRéalisé = TimeSpan.Zero;
+            DuréeTravailRéalisé = 0;
+        }
+
+        public Taches(DateTime dateInitiation) :this()
+        {
+            DateDébut = dateInitiation;
         }
         #endregion
-
+        
         #region Méthodes
         public void Annulation()
         {
@@ -33,17 +38,30 @@ namespace ProjetConsole
     public class TachesProduction : Taches
     {
         #region Propriétés
-        public TimeSpan DuréeTravailPrévue { get; }
-        public TimeSpan DuréeTravailRéstante { get; set; }
+        public int DuréeTravailPrévue { get; }
+        public int DuréeTravailRéstante { get; set; }
         public bool TacheTerminée
         {
             get
             {
-                if (DuréeTravailRéstante == TimeSpan.Zero)
+                if (DuréeTravailRéstante == DateDébut.)
                     return true;
                 else
                     return false;
             }
+        }
+        #endregion
+
+        #region Constructeur
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="duréePrévue"></param>
+        /// <param name="duréeRéstante"></param>
+        public TachesProduction(int duréePrévue, int duréeRéstante)
+        {
+            DuréeTravailPrévue = duréePrévue;
+            DuréeTravailRéstante = duréeRéstante;
         }
         #endregion
     }
@@ -51,7 +69,7 @@ namespace ProjetConsole
     public class TachesAnnexes : Taches
     {
         #region Propriétés
-        public TimeSpan DuréeTravailRéaliséMensuelle
+        public int DuréeTravailRéaliséMensuelle
         {
             get
             {
@@ -60,8 +78,15 @@ namespace ProjetConsole
             set
             {
                 if (DateTime.Today == new DateTime(DateTime.Today.Year, DateTime.Today.Month, 01, 00, 00, 00)) // Si on est le 1er du mois à 00h00min00sec alors DuréeRéaliséeMensuel = 0.
-                    DuréeTravailRéaliséMensuelle = TimeSpan.Zero;
+                    DuréeTravailRéaliséMensuelle = 0;
             }
+        }
+        #endregion
+
+        #region Constructeur
+        public TachesAnnexes() :base()
+        {
+            DuréeTravailRéaliséMensuelle = 0;
         }
         #endregion
     }
